9678b76c92a467798dcf945bc132f22cbf253c25
ReaLNeroM <vlade.maksimovski@gmail.com>
2018-08-30 23:28:28 -0400

    Added customizable length

--- main.py
+++ main.py
@@ -1,49 +1,61 @@
 import turtle
 
 def do_recursively(step, endStep):
-    if step == endStep:
-        turtle.forward(50)
-        return
+	if step == endStep:
+		turtle.forward(50)
+		return
 
-    do_recursively(step + 1, endStep)
-    turtle.right(60)
-    do_recursively(step + 1, endStep)
-    turtle.left(60)
-    turtle.left(60)
-    do_recursively(step + 1, endStep)
-    turtle.right(60)
-    do_recursively(step + 1, endStep)
+	do_recursively(step + 1, endStep)
+	turtle.right(60)
+	do_recursively(step + 1, endStep)
+	turtle.left(60)
+	turtle.left(60)
+	do_recursively(step + 1, endStep)
+	turtle.right(60)
+	do_recursively(step + 1, endStep)
 
 
-def do_iteratively(endStep):
-    l = [0]
-    while len(l) > 0:
-        fr = l[-1]
-        del l[-1]
+def do_iteratively(endStep, length):
+	l = [0] #l is an update list
+	size = [length]
+	while len(l) > 0:
+		fr = l[-1]
+		del l[-1]
 
-        if fr == endStep:
-            turtle.forward(50)
-            continue
-        elif fr == -2:
-            turtle.right(60)
-            continue
-        elif fr == -1:
-            turtle.left(60)
-            continue
+		if fr == endStep:
+			frlength = size[-1]
+			del size[-1]
+			turtle.forward(frlength)
+			continue
+		elif fr == -2:
+			turtle.right(60)
+			continue
+		elif fr == -1:
+			turtle.left(60)
+			continue
 
-        l.append(fr + 1)
-        l.append(-2) #turtle.right(60)
-        l.append(fr + 1)
-        l.append(-1) #turtle.left(60)
-        l.append(-1)
-        l.append(fr + 1)
-        l.append(-2)
-        l.append(fr + 1)
+		
+		frlength = size[-1]
+		del size[-1]
+
+		l.append(fr + 1)
+		size.append(length / 3.0)
+		l.append(-2) #turtle.right(60)
+		l.append(fr + 1)
+		size.append(length / 3.0)
+		l.append(-1) #turtle.left(60)
+		l.append(-1)
+		l.append(fr + 1)
+		size.append(length / 3.0)
+		l.append(-2)
+		l.append(fr + 1)
+		size.append(length / 3.0)
 
 
 endStep = input("To what depth?")
+length = 100.0
 # do_recursively(0, endStep)
-do_iteratively(endStep)
+do_iteratively(endStep, length)
 
 while True:
-    pass
+	pass
